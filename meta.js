e8c3fcfd2e14ba25a079267062aae53da792e27b
xsota <git@xsota.com>
2018-05-04 21:19:39 +0900

    init

---
+++ meta.js
@@ -0,0 +1,25 @@
+module.exports = {
+  helpers: {
+    escape: function(value) {
+      return value.replace(/'/g, '&apos;');
+    }
+  },
+  prompts: {
+    name: {
+      'type': 'string',
+      'required': true,
+      'message': 'Project name'
+    },
+    description: {
+      'type': 'string',
+      'required': false,
+      'message': 'Project description',
+      'default': 'Nuxt.js project'
+    },
+    author: {
+      'type': 'string',
+      'message': 'Author'
+    },
+  },
+  completeMessage: '{{#inPlace}}To get started:\n\n  yarn\n  yarn dev{{else}}To get started:\n\n  cd {{destDirName}}\n  yarn\n  yarn dev{{/inPlace}}'
+};
